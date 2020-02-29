121032d4d3e39ad16cf54e0ae9230fd02e3d0aa5
EpicNav <navjot222@hotmail.com>
2020-02-29 23:21:25 +0530

    New Project

---
+++ VideoRentalApp/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace VideoRentalApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ VideoRentalApp/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace VideoRentalApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
